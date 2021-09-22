d4e0fc1da79f91faa1a9b50a3dbd29fd0f86960a
GavinVikramJ <anbuleader9@gmail.com>
2021-09-22 11:08:12 +0700

    Initial commit

---
+++ Week1.php
@@ -0,0 +1,31 @@
+<?php 
+$nama = "Gavin Vikram Juniwan" ;
+$kelas = "MI20B" ;
+$alamat = "Sukarindik" ;
+$ttl = "19 Juni 2002" ;
+$agama = "Islam"
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>WEEK 1 - Gavin Vikram Juniwan</title>
+</head>
+<body>    
+    <?php
+        echo "BIODATA";
+        echo "<br>" ; //Untuk memberikan spasi
+        echo "<br>" ;
+        echo "Nama : ".$nama ;
+        echo "<br>" ;
+        echo "kelas : ".$kelas ; 
+        echo "<br>" ;
+        echo "Alamat : ".$alamat ;
+        echo "<br>" ;
+        echo "Tempat Tanggal Lahir :".$ttl ;
+        echo "<br>" ;
+        echo "Agama : ".$agama ;
+        
+
+    ?>
+</body>
+</html>
